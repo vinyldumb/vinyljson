ba5a8d278ea43b172ee2dd1b2918a5392fe79d07
vinyldumb <mortirfan@gmail.com>
2025-02-28 21:32:05 +1100

    add docs to IJsonSerializable

--- vinyl/json/IJsonSerializable.hx
+++ vinyl/json/IJsonSerializable.hx
@@ -1,4 +1,7 @@
 package vinyl.json;
 
+/**
+ * Automatically adds @:rtti metadata to the class if it has not got it
+ */
 @:autoBuild(vinyl.json._internal.Macros.addRtti())
 interface IJsonSerializable {}
